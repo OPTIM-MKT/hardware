02821e48624c17317f82ff6fbbbb45ce1844c1fe
Leo0114 <leonel0614@gmail.com>
2026-06-01 16:04:46 -0600

    update: Zebra update

---
+++ src/constants/singlebrand.ts
@@ -0,0 +1,10 @@
+import zebra from "@/assets/images/brands/zebra.webp";
+
+export const zebraData = {
+  image: zebra,
+  title: "Zebra",
+  badgeText: "Distribuidor autorizado",
+  p: "Zebra es el líder mundial en soluciones de identificación, impresión y captura de datos. Con más de 50 años de experiencia, Zebra ofrece una amplia gama de productos y soluciones para satisfacer las necesidades de su negocio.",
+  href: "/zebra.html",
+  cta: "Conoce más",
+};

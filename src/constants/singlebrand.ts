01e17c58cd9b8b7c25a88aaca106c0c5b5f5bca4
Leo0114 <leonel0614@gmail.com>
2026-06-01 16:07:57 -0600

    fixed link

--- src/constants/singlebrand.ts
+++ src/constants/singlebrand.ts
@@ -5,6 +5,6 @@ export const zebraData = {
   title: "Zebra",
   badgeText: "Distribuidor autorizado",
   p: "Zebra es el líder mundial en soluciones de identificación, impresión y captura de datos. Con más de 50 años de experiencia, Zebra ofrece una amplia gama de productos y soluciones para satisfacer las necesidades de su negocio.",
-  href: "/zebra.html",
+  href: "/zebra",
   cta: "Conoce más",
 };

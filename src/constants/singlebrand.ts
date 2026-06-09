b072cc23e555cec59f65bb50294f8feed6fc3a88
Leo0114 <leonel0614@gmail.com>
2026-06-09 11:22:11 -0600

    update : 3dconn

--- src/constants/singlebrand.ts
+++ src/constants/singlebrand.ts
@@ -1,4 +1,5 @@
 import zebra from "@/assets/images/brands/zebra.webp";
+import connexion from "@/assets/images/brands/3dConnexion.webp";
 
 export const zebraData = {
   image: zebra,
@@ -6,5 +7,14 @@ export const zebraData = {
   badgeText: "Distribuidor autorizado",
   p: "Zebra es el líder mundial en soluciones de identificación, impresión y captura de datos. Con más de 50 años de experiencia, Zebra ofrece una amplia gama de productos y soluciones para satisfacer las necesidades de su negocio.",
   href: "/zebra",
+  cta: "Conoce más",
+};
+
+export const DconnData = {
+  image: connexion,
+  title: "3Dconnexion",
+  badgeText: "Distribuidor autorizado",
+  p: "Desde controladores avanzados de CAD en 3D hasta ratones virtuales y más, 3Dconnexion transforma su flujo de trabajo digital, liberándolo de los límites del teclado y el mouse tradicionales, para que pueda diseñar, crear y explorar con una sensación natural e intuitiva.",
+  href: "/3Dconnexion",
   cta: "Conoce más",
 };
